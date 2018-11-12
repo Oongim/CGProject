ba7b35d9e7e2c448bbbd484500e06096e2029309
kshg1324(laptop) <kshg1324@naver.com>
2018-11-12 15:11:04 +0900

    배 이동 구현
    여러가지 상황에서 배 이동 구현
    앞으로 작살 하면됨

--- KHM/Project8/소스.cpp
+++ KHM/Project8/소스.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #define PI 3.141592
 #define RADIAN PI/180
+#define WORLD_SCALE 4000
+enum Direction{LEFT = -1 , KEEP, RIGHT};
 using namespace std;
 
 static double prev_mx = 0;
@@ -18,13 +20,13 @@ namespace KHM
 	int MODE_OF_VIEW = 3; // 1 = 1인칭(작살), 3 = 3인칭 
 	struct BOAT 
 	{
+		bool is_forward = false;
+		int direction = KEEP;
+		bool is_breaking = false;
 		float HULL_RADIAN = 0;
 		float MOVE_RADIAN = 0;
-		float x = 0;
-		float y = 0;
-		float z = 0;
-		float phi = 90;
-		float theta = 0;
+		float speed = 0.0;
+		float velocity = 0.0002;
 	};
 	BOAT Boat;
 
@@ -35,9 +37,42 @@ namespace KHM
 		float y = 0;
 		float z = 0;
 		float t = 0;
+		bool is_hit = false; 
 		bool is_fired = false;
 	};
-	HARPOON Harpoon;
+	struct Node
+	{
+		HARPOON Harpoon;
+		Node * next;
+	};
+	Node * Head;
+	void Insert()
+	{
+		Node *New = new Node;
+		
+		New->Harpoon.x = 0;
+		New->Harpoon.y = 0;
+		New->Harpoon.z = 0;
+		New->Harpoon.HULL_RADIAN = 0;
+		New->Harpoon.is_hit = false;
+		New->Harpoon.is_fired = false;
+		New->next = nullptr;
+		Node *curr = Head;
+		while (Head->next != nullptr)
+		{
+			curr = curr->next;
+		}
+		curr->next = New;
+	}
+	void shot_Harpoon()
+	{
+		Node *curr = Head;
+		while (Head->next != nullptr)
+		{
+			curr = curr->next;
+		}
+		curr->Harpoon.is_fired = true;
+	}
 }
 
 GLvoid drawScene(GLvoid);
@@ -46,6 +81,8 @@ GLvoid Reshape(int w, int h);
 Camera<float3> m_camera{ 600.f / 800.f };
 
 void Keyboard(unsigned char key, int x, int y);
+void UpKeyboard(unsigned char key, int x, int y);
+
 void Timerfunction(int value);
 
 double Eye_x = 0.0;
@@ -63,19 +100,12 @@ double Camera_z = 900.0;
 
 
 GLdouble rotateWC[16]
-////={1,0,0,0,
-////  0,1,0,0,
-////  0,0,1,0,
-////  0,0,0,1};
 = { 1,0,0,0,
   0,1,0,0,
   0,0,1,0,
   0,0,0,1 };
 
-//GLdouble rotateWC[9]
-//= { 1,0,0,
-//0,1,0,
-//0,0,1 };
+
 void Mouse(int button, int state, int x, int y)
 {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
@@ -148,27 +178,25 @@ void draw_COORD()
 void draw_BOTTOM()
 {
 	glPushMatrix();
-	glTranslated(0, -2000, 0);
 	glColor4f(0.66f, 0.66f, 0.66f, 1.0f);
-	glutSolidSphere(1500, 75, 75);
+	glutWireSphere(1500, 75, 75);
 	glPopMatrix();
 }
 void draw_SEA()
 {
 	glPushMatrix();
-	glTranslated(0, -2000, 0);
 	glColor4f(0.25f, 0.85f, 0.92f, 0.3f);
-	glutSolidSphere(2000, 100, 100);
+	glutWireSphere(WORLD_SCALE, 100, 100);
 	glPopMatrix();
 }
 void draw_BOAT()
 {
 	glPushMatrix();
-	glTranslated(KHM::Boat.x, KHM::Boat.y, KHM::Boat.z);
+	//glTranslated(KHM::Boat.x, KHM::Boat.y, KHM::Boat.z);
+	//glRotated(KHM::Boat.phi, 0, 1, 0); 
 	glRotated(KHM::Boat.HULL_RADIAN , 0, 1, 0); //배 방향
-	glRotated(KHM::Boat.phi, 0, 0, 1); 
 	glColor4f(1.0f, 0.5f, 0.5f, 1.0f);
-	glScalef(1.0f, 2.0f, 1.0f);
+	glScalef(1.0f, 1.0f, 2.0f);
 	glutSolidCube(100);
 	glPopMatrix();
 }
@@ -176,9 +204,7 @@ void draw_BOAT()
 void draw_HARPOON()
 {
 	glPushMatrix();
-	glTranslated(KHM::Harpoon.x, KHM::Harpoon.y + 100 * cos(KHM::Boat.phi) * cos(KHM::Boat.theta), KHM::Harpoon.z * sin(KHM::Boat.theta));
-	//glRotated(KHM::Boat.phi, 0, 0, 1);
-	//glRotated(KHM::Harpoon.HULL_RADIAN, 0, 1, 0);
+	//glTranslated(KHM::Harpoon.x, KHM::Harpoon.y + 100, KHM::Harpoon.z);
 
 	glColor3f(1.0, 0.0, 1.0);
 	/*********************작살 봉*********************/
@@ -201,47 +227,25 @@ void draw_HARPOON()
 	glPopMatrix();
 }
 
-void draw_Harpoon(float x, float y, float z)
-{	//나중에 각도 추가하거나 알아서 계산해야 될듯
-
-	glPushMatrix(); {
-		glTranslatef(x, y, z);
-		glRotatef(0, 0.0, 1.0, 0.0);
-
-		glColor3f(1.0, 0.0, 1.0);
-		/*********************작살 봉*********************/
-		glPushMatrix(); {
-			glScalef(1.0, 1.0, 50.0);           //봉길이 50
-			glutSolidTorus(0.5, 1, 20, 20);
-		}glPopMatrix();
-		/*********************작살 촉*********************/
-		glColor3f(1.0, 0.0, 0.0);
-		glPushMatrix(); {
-			glTranslatef(0, 0, 25);
-			glScalef(1.0, 1.0, 1.0);
-			glutSolidCone(3, 5, 10, 10);
-			glTranslatef(0, 0, 3);
-			glutSolidCone(3, 5, 10, 10);
-			glTranslatef(0, 0, 3);
-			glutSolidCone(3, 5, 10, 10);
-		}glPopMatrix();
-	}glPopMatrix();
+void initialize()
+{
+	KHM::Head->next = nullptr;
 }
-
 void main(int argc, char *argv[])
 {
-	m_camera.Initialize(float3{ KHM::Boat.x,KHM::Boat.y,KHM::Boat.z }, 900, 1, 9999, 90);
+	initialize();
+	m_camera.Initialize(float3{ 0,0,0 }, 1200, 1, 9999, 90);
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH); // 디스플레이 모드 설정
 	glutInitWindowPosition(100, 100); // 윈도우의 위치지정
 	glutInitWindowSize(800, 600); // 윈도우의 크기 지정
-
-	glutCreateWindow("실습16"); // 윈도우 생성 (윈도우 이름)
+	glutCreateWindow("포경"); // 윈도우 생성 (윈도우 이름)
 	glutDisplayFunc(drawScene); // 출력 함수의 지정
 	glutMouseFunc(Mouse);
 	glutMotionFunc(Motion);
 	glutReshapeFunc(Reshape);
 	glutKeyboardFunc(Keyboard);                  // 키보드 입력 콜백 함수
+	glutKeyboardUpFunc(UpKeyboard);                  // 키보드 입력 콜백 함수
 	glutTimerFunc(10, Timerfunction, 1);         // 타이머 함수 설정
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
@@ -256,17 +260,21 @@ GLvoid drawScene(GLvoid)
 	//////////////////////////////////////////////
 	m_camera.LookAt();
 
-
-	//glMatrixMode(GL_MODELVIEW);
-	//glLoadIdentity();
-
-
 	//draw_COORD();
+	draw_BOAT();
+	glPushMatrix();
+	glTranslated(0, -WORLD_SCALE, 0);
+	glMultMatrixd(rotateWC);
+
+	glPushMatrix();
+	glTranslated(0, WORLD_SCALE, 0);
+	glutSolidCube(100);
+	glPopMatrix();
+
+	//draw_HARPOON();
 	draw_BOTTOM();
 	draw_SEA();
-	draw_BOAT();
-	draw_HARPOON();
-
+	glPopMatrix();
 	glFlush(); // 화면에 출력하기
 }
 GLvoid Reshape(int w, int h)
@@ -286,48 +294,8 @@ GLvoid Reshape(int w, int h)
 
 void Keyboard(unsigned char key, int x, int y)
 {
-	if (key == 'w')
-	{
-		//KHM::Boat.x += sin(KHM::Boat.MOVE_RADIAN) * 3;
-		//KHM::Boat.z += cos(KHM::Boat.MOVE_RADIAN) * 3;
-		//theta++;
-		KHM::Boat.phi++;
-		KHM::Harpoon.z = KHM::Boat.z;
-		KHM::Harpoon.y = KHM::Boat.y;
-		KHM::Harpoon.x = KHM::Boat.x;
-		cout << KHM::Boat.x << " " << KHM::Boat.y << " " << KHM::Boat.z << " " << KHM::Boat.HULL_RADIAN << " " << KHM::Boat.MOVE_RADIAN << endl;
-	}
+	int mod = 0;
 
-	//void draw_basic_Whale(float r, float phi, float theta, GLdouble arr[])
-	//{
-	//	//r 원점으로부터의 거리, phi x와 z의 각도, theta x,z평면과 y의 각도, arr 회전을 쓸 배열이 들어갈 듯
-	//	float x = r * cos(phi*RADIAN) * cos(theta*RADIAN);
-	//	float y = r * sin(phi*RADIAN);
-	//	float z = r * cos(phi*RADIAN) * sin(theta*RADIAN);
-
-	//if (-PI * 0.5f < m_horizontal + h && m_horizontal + h < PI * 0.5f)
-	if (key == 'a')
-	{
-		KHM::Boat.HULL_RADIAN++;
-		//KHM::Harpoon.radian++;
-		KHM::Boat.theta++;
-		cout << KHM::Boat.x << " " << KHM::Boat.y << " " << KHM::Boat.z << " " << KHM::Boat.HULL_RADIAN << " " << KHM::Boat.MOVE_RADIAN << endl;
-	}
-	//if (key == 's')
-	//{
-	//	KHM::Boat.z -= cos(KHM::Boat.MOVE_RADIAN) * 3;
-	//	KHM::Boat.x -= sin(KHM::Boat.MOVE_RADIAN) * 3;
-	//	KHM::Harpoon.z = KHM::Boat.z;
-	//	KHM::Harpoon.x = KHM::Boat.x;
-	//	cout << KHM::Boat.x << " " << KHM::Boat.y << " " << KHM::Boat.z << " " << KHM::Boat.HULL_RADIAN << " " << KHM::Boat.MOVE_RADIAN << endl;
-	//}
-	if (key == 'd')
-	{
-		KHM::Boat.HULL_RADIAN--;
-		//KHM::Harpoon.radian--;
-		KHM::Boat.theta--;
-		cout << KHM::Boat.x << " " << KHM::Boat.y << " " << KHM::Boat.z << " " << KHM::Boat.HULL_RADIAN << " " << KHM::Boat.MOVE_RADIAN << endl;
-	}
 	switch (key)
 	{
 	case 'q':
@@ -335,38 +303,53 @@ void Keyboard(unsigned char key, int x, int y)
 		break;
 	case 'i':
 		glPushMatrix();
-		KHM::Harpoon.y = 100;
-		KHM::Harpoon.z = KHM::Boat.z;
-		KHM::Harpoon.x = KHM::Boat.x;
-		KHM::Harpoon.t = 0;
-		KHM::Harpoon.is_fired = false;
-
 		glPopMatrix();
 		break;
-	//case 'w':
-	//	//BOAT_z+=3;
-	//	KHM::Boat.z += cos(KHM::Boat.MOVE_RADIAN) * 3;
-	//	KHM::Boat.x += sin(KHM::Boat.MOVE_RADIAN) * 3;
-	//	cout << KHM::Boat.x << " " << KHM::Boat.y << " " << KHM::Boat.z << " " << KHM::Boat.HULL_RADIAN << " " << KHM::Boat.MOVE_RADIAN << endl;
-	//	break;
-	//case 'a':
-	//	KHM::Boat.HULL_RADIAN++;
-	//	cout << KHM::Boat.x << " " << KHM::Boat.y << " " << KHM::Boat.z << " " << KHM::Boat.HULL_RADIAN << " " << KHM::Boat.MOVE_RADIAN << endl;
-	//	break;
-	//case 's':
-	//	//BOAT_z-=3;
-	//	KHM::Boat.z -= cos(KHM::Boat.MOVE_RADIAN) * 3;
-	//	KHM::Boat.x -= sin(KHM::Boat.MOVE_RADIAN) * 3;
-	//	cout << KHM::Boat.x << " " << KHM::Boat.y << " " << KHM::Boat.z << " " << KHM::Boat.HULL_RADIAN << " " << KHM::Boat.MOVE_RADIAN << endl;
-	//	break;
-	//case 'd':
-	//	KHM::Boat.HULL_RADIAN--;
-	//	cout << KHM::Boat.x << " " << KHM::Boat.y << " " << KHM::Boat.z << " " << KHM::Boat.HULL_RADIAN << " " << KHM::Boat.MOVE_RADIAN << endl;
-	//	break;
+	case 'w':
+		KHM::Boat.is_forward = true;
+		mod = glutGetModifiers();
+		if (mod == 'a')
+		{
+			glPushMatrix();
+			glRotatef(-KHM::Boat.speed, 0.0, 1.0, 0.0);
+			glMultMatrixd(rotateWC);
+			glGetDoublev(GL_MODELVIEW_MATRIX, rotateWC);
+			glPopMatrix();
+		}
+		else if (mod == 'd')
+		{
+			glPushMatrix();
+			glRotatef(KHM::Boat.speed, 0.0, 1.0, 0.0);
+			glMultMatrixd(rotateWC);
+			glGetDoublev(GL_MODELVIEW_MATRIX, rotateWC);
+			glPopMatrix();
+		}		
+		
+		break;
+	case 'a':
+		KHM::Boat.direction = LEFT;
+		mod = glutGetModifiers();
+		if (mod == 'w')
+		{
+			KHM::Boat.is_forward = true;
+		}
+		break;
+	case 's':
+		if (KHM::Boat.speed > 0.0)
+		{
+			KHM::Boat.is_breaking = true;
+		}
+		break;
+	case 'd':
+		KHM::Boat.direction = RIGHT;
+		mod = glutGetModifiers();
+		if (mod == 'w')
+		{
+			KHM::Boat.is_forward = true;
+		}
+		break;
 	case 'f':
-		//KHM::Harpoon.x;
-		KHM::Harpoon.is_fired = true;
-		cout << KHM::Boat.x << " " << KHM::Boat.y << " " << KHM::Boat.z << " " << KHM::Boat.HULL_RADIAN << " " << KHM::Boat.MOVE_RADIAN << endl;
+		shot_Harpoon();
 		break;
 	case '1':
 		m_camera.Initialize_radian();
@@ -385,9 +368,27 @@ void Keyboard(unsigned char key, int x, int y)
 	glutPostRedisplay();
 }
 
-void Timerfunction(int value)         // 이 함수는 한 번만 실행되므로 지속적인 애니메이션을 위해서는
+void UpKeyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'w':
+		KHM::Boat.is_forward = false;
+		break;
+	case 'a':
+		KHM::Boat.direction = KEEP;
+		break;
+	case 'd':
+		KHM::Boat.direction = KEEP;
+		break;
+	case 's':
+		KHM::Boat.is_breaking = false;
+		break;
+	}
+}
+
+void Timerfunction(int value) 
 {   
-	// 타이머 함수 내에 타이머를 다시 호출해야 한다.
 	if (KHM::Boat.HULL_RADIAN > 360)
 	{
 		KHM::Boat.HULL_RADIAN = KHM::Boat.HULL_RADIAN - 360;
@@ -396,12 +397,13 @@ void Timerfunction(int value)         // 이 함수는 한 번만 실행되므로 지속적인 �
 	KHM::Boat.MOVE_RADIAN = KHM::Boat.MOVE_RADIAN * RADIAN;
 	if (KHM::MODE_OF_VIEW == 3)
 	{
-		m_camera.Initialize(float3{ KHM::Boat.x,KHM::Boat.y,KHM::Boat.z }, 900, 1, 9999, 90);
+		//m_camera.Initialize(float3{ KHM::Boat.x,KHM::Boat.y,KHM::Boat.z }, 1800, 1, 9999, 90);
 	}
 	else if (KHM::MODE_OF_VIEW == 1)
 	{
 		m_camera.Initialize(float3{ KHM::Harpoon.x,KHM::Harpoon.y,KHM::Harpoon.z }, 30, 1, 9999, 90);
 	}
+	curr
 	if (KHM::Harpoon.is_fired == true)
 	{
 		if (KHM::Harpoon.y > 0)
@@ -415,13 +417,47 @@ void Timerfunction(int value)         // 이 함수는 한 번만 실행되므로 지속적인 �
 			//KHM::Harpoon.x += sin(KHM::Boat.MOVE_RADIAN) * 3;
 		}
 	}
-	KHM::Boat.x = 2000 * cos(KHM::Boat.phi*RADIAN) * cos(KHM::Boat.theta*RADIAN);
-	KHM::Boat.y = 2000 * sin(KHM::Boat.phi*RADIAN) - 2000;
-	KHM::Boat.z = 2000 * cos(KHM::Boat.phi*RADIAN) * sin(KHM::Boat.theta*RADIAN);
+	glPushMatrix(); {
+		if (KHM::Boat.is_forward == true)
+		{
+			if (KHM::Boat.speed < 0.1)
+			{
+				KHM::Boat.speed += KHM::Boat.velocity;
+			}
+		}
+		else
+		{
+			if (KHM::Boat.speed > 0.0)
+			{
+				KHM::Boat.speed -= KHM::Boat.velocity * 2;
+				if (KHM::Boat.is_breaking == true)
+				{
+					KHM::Boat.speed -= KHM::Boat.velocity * 3;
+				}
+				if (KHM::Boat.speed <= 0)
+				{
+					KHM::Boat.speed = 0;
+				}
+			}
 
-	//KHM::Harpoon.x = 2000 * cos(KHM::Boat.phi*RADIAN) * cos(KHM::Boat.theta*RADIAN);
-	//KHM::Harpoon.y = 2000 * sin(KHM::Boat.phi*RADIAN) - 2000;
-	//KHM::Harpoon.z = 2000 * cos(KHM::Boat.phi*RADIAN) * sin(KHM::Boat.theta*RADIAN);
+		}
+		glRotatef(-KHM::Boat.speed, 1.0, 0.0, 0.0);
+		glMultMatrixd(rotateWC);
+		glGetDoublev(GL_MODELVIEW_MATRIX, rotateWC);
+	}glPopMatrix();
+	glPushMatrix(); {
+		if (KHM::Boat.direction == LEFT)
+		{
+			glRotatef(-(KHM::Boat.speed * 3 + 0.3), 0.0, 1.0, 0.0);
+		}
+		else if (KHM::Boat.direction == RIGHT)
+		{
+			glRotatef((KHM::Boat.speed * 3 + 0.3), 0.0, 1.0, 0.0);
+
+		}
+		glMultMatrixd(rotateWC);
+		glGetDoublev(GL_MODELVIEW_MATRIX, rotateWC);
+	}glPopMatrix();
 	glutPostRedisplay();                  // 화면 재출력
 	glutTimerFunc(10, Timerfunction, 1);      // 타이머함수 재설정
 }
